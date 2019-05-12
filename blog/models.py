00f4f38f084c6276de04627c892e7fbb06744222
HuaiZ <apokar@163.com>
2019-05-12 16:33:57 +0800

    自动生成摘要

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+import markdown
+from django.utils.html import strip_tags
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -49,3 +50,22 @@ class Post(models.Model):
     tags = models.ManyToManyField(Tag, blank=True)  # ManyToManyField，表明这是多对多的关联关系
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    body = models.TextField()
+    excerpt = models.CharField(max_length=200, blank=True)
+
+    def save(self, *args, **kwargs):
+        # 如果没有填写摘要
+        if not self.excerpt:
+            # 首先实例化一个 Markdown 类，用于渲染 body 的文本
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            # 先将 Markdown 文本渲染成 HTML 文本
+            # strip_tags 去掉 HTML 文本的全部 HTML 标签
+            # 从文本摘取前 54 个字符赋给 excerpt
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+
+        # 调用父类的 save 方法将数据保存到数据库中
+        super(Post, self).save(*args, **kwargs)
